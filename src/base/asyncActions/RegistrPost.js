694292a917a0002a1d20d8025cfaf076b3bd96bb
AlexandrKruglenko <fredwolk6@gmail.com>
2022-07-08 18:29:39 +0300

    Update RegistrPost.js

--- src/base/asyncActions/RegistrPost.js
+++ src/base/asyncActions/RegistrPost.js
@@ -1,13 +1,13 @@
-import { RegisterAction } from "../Reducers/registerReducer"
+import {RegisterAction} from "../Reducers/registerReducer"
 import axios from "axios";
-const url = 'https://api.telemed.dev-h.ru/v1/';
+import { defaultUrl } from '../configUrl'
+
 export const axiosRegister = (obj) => {
-    return async function (dispatch) {
-        const response = await axios.post(`${url}user`,{...obj}).then(response => {
-            if (response.data.status == true) {
-                console.log(obj);
-                dispatch(RegisterAction(obj))
-            }
-        });
+  return async function (dispatch) {
+    const response = await axios.post(`${defaultUrl}user`, {...obj})
+    if (response.data.status) {
+      console.log(obj);
+      dispatch(RegisterAction(obj))
     }
+  }
 }
