9b9b1a5c0f9e4a9bd4fabe65d9015d8b926b06bf
AlexandrKruglenko <fredwolk6@gmail.com>
2022-07-12 15:35:48 +0300

    Фикс редиректа после логина

--- src/Pages/Layout/Header/UserMenu/UserMenu.jsx
+++ src/Pages/Layout/Header/UserMenu/UserMenu.jsx
@@ -1,53 +1,89 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import s from "./UserMenu.module.css";
 import message_img from "../../../../img/coolicon.png";
 import avatar from "../../../../img/avatar.png";
 import arrow from "../../../../img/arrow.png";
-import {Link} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {getShortInfo} from "../../../../base/asyncActions/getMainPageInfo";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getShortInfo } from "../../../../base/asyncActions/getMainPageInfo";
 
 const UserMenu = () => {
-  const dispatch = useDispatch()
+  const logout = () => {
+    localStorage.clear();
+  };
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(getShortInfo())
-  }, [])
-  const [show, setShow] = useState(false)
+    dispatch(getShortInfo());
+  }, []);
+
+  const [show, setShow] = useState(false);
+
   return (
     <div className={s.jare}>
       <div className={s.Header_profile}>
         <div className={s.Profile_logo}>
-          <img src={message_img} alt=""/>
+          <img src={message_img} alt="" />
         </div>
         <div onClick={() => setShow(true)} className={s.Profile_text}>
           <div className={s.Profile_avatar}>
-            <img src={avatar} alt=""/>
+            <img src={avatar} alt="" />
           </div>
           <div className={s.Profile_menu}>
             <p>Смирнов В. В.</p>
-            <img src={arrow} alt=""/>
+            <img src={arrow} alt="" />
           </div>
         </div>
       </div>
-      {
-        show
-        &&
+      {show && (
         <div onClick={() => setShow(false)} className={s.MenuBack}>
           <div className={s.MenuWrap}>
             <ul className={s.MenuList}>
-              <li><Link className={`${s.MenuLink} ${s.LinkBorder}`} to={'consultation'}>Личные данные</Link></li>
-              <li><Link className={s.MenuLink} to={'/'}>Баланс</Link></li>
-              <li><Link className={s.MenuLink} to={'/'}>Проверка оборудования</Link></li>
-              <li><Link className={s.MenuLink} to={'/'}>Анкета</Link></li>
-              <li><Link className={s.MenuLink} to={'/'}>Медицинская карта</Link></li>
-              <li><Link className={s.MenuLink} to={'/'}>Результаты исследований</Link></li>
-              <li><Link className={s.MenuLink} to={'/'}>Выйти</Link></li>
+              <li>
+                <Link
+                  className={`${s.MenuLink} ${s.LinkBorder}`}
+                  to={"consultation"}
+                >
+                  Личные данные
+                </Link>
+              </li>
+              <li>
+                <Link className={s.MenuLink} to={"/"}>
+                  Баланс
+                </Link>
+              </li>
+              <li>
+                <Link className={s.MenuLink} to={"/"}>
+                  Проверка оборудования
+                </Link>
+              </li>
+              <li>
+                <Link className={s.MenuLink} to={"/"}>
+                  Анкета
+                </Link>
+              </li>
+              <li>
+                <Link className={s.MenuLink} to={"/"}>
+                  Медицинская карта
+                </Link>
+              </li>
+              <li>
+                <Link className={s.MenuLink} to={"/"}>
+                  Результаты исследований
+                </Link>
+              </li>
+              <li>
+                <Link className={s.MenuLink} onClick={() => logout()} to={"/"}>
+                  Выйти
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
-      }
+      )}
     </div>
   );
 };
 
-export {UserMenu};
+export { UserMenu };
--- src/Pages/Views/Register/Register.jsx
+++ src/Pages/Views/Register/Register.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import style from './register.module.css'
-import {Navigate} from "react-router-dom";
-import {RegisterForm} from "../../../Components/Forms/RegisterForm";
+import React from "react";
+import style from "./register.module.css";
+import { Navigate } from "react-router-dom";
+import { RegisterForm } from "../../../Components/Forms/RegisterForm";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Register = () => {
-  const newUser = localStorage.getItem('isNewUser')
+  const newUser = localStorage.getItem("isNewUser");
   return (
-    <div className={'Container'}>
-    {!newUser ? <Navigate to={'/main'}/> : false}
+    <div className={"Container"}>
+      {newUser == "false" ? <Navigate to={"/main"} /> : false}
       <div className={style.form_container}>
-        <h1 className={style.title}>Завершите регистрацию, чтобы получить доступ к услугам</h1>
-        <RegisterForm/>
+        <h1 className={style.title}>
+          Завершите регистрацию, чтобы получить доступ к услугам
+        </h1>
+        <RegisterForm />
       </div>
     </div>
   );
 };
 
-export {Register};
+export { Register };
