0cdb34bb85caa83033005ed5e95abdc41486b032
AlexandrKruglenko <fredwolk6@gmail.com>
2022-07-19 12:40:40 +0300

    Правки

--- src/Pages/Views/Post_review/Post_review.jsx
+++ src/Pages/Views/Post_review/Post_review.jsx
@@ -1,96 +1,123 @@
 import React from "react";
-import s from './Post_review.module.css';
-import star from '../../../img/Rating_Star.png';
+import s from "./Post_review.module.css";
+import star from "../../../img/Rating_Star.png";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { axiosReviews } from "../../../base/asyncActions/getReviews";
 import SelectReview from "../../../Components/Select/SelectReview/SelectReview";
 import StarComponent from "./StarComponent";
-import { Star1Action, Star2Action, Star3Action, Star4Action, Star5Action } from "../../../base/Reducers/reviewsReducer";
+import {
+  Star1Action,
+  Star2Action,
+  Star3Action,
+  Star4Action,
+  Star5Action,
+} from "../../../base/Reducers/reviewsReducer";
 const PostRewiew = () => {
-    let dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(axiosReviews(4));
-    }, []);
-    const review = useSelector((state) => state.review);
-    const sendForm = async (e) => {
-        e.preventDefault()
-        const data = await new FormData(e.target);
-        console.log({...data});
-      }
-    return (
-        <div className={s.Review_full}>
-            <div className={s.Review_title}>
-                <h1 className={s.Font_size24}>Оставить отзыв</h1>
-            </div>
-            <div className={s.Doctor_infos}>
-                <div className={s.Doctor_avatar}>
-                    <div className={s.Doctor_avatar_img}>
-                        <img src={review.photo} alt="" />
-                    </div>
-                </div>
-                <div className={s.Doctor_info + " " + s.black}>
-                    <p className={s.gray + " " + s.Font_size14}>{review.specialization.join(' • ')}</p>
-                    <h2 className={s.Font_size24}>{review.firstname + " " + review.lastname + " " + review.secondname}</h2>
-                    <p className={s.Staj + " " + s.Font_size14}>{review.regalia.join(' • ')}</p>
-                </div>
-            </div>
-            <div className={s.Grade}>
-                <ul>
-                    <li>
-                        <b className={s.Font_size24}>Тщательное обследование</b>
-                        <div className={s.Doctor_avatar_info + " " + s.black}>
-                            <StarComponent func={Star1Action} />
-                        </div>
-                    </li>
-                    <li>
-                        <b className={s.Font_size24}>Эффективность лечения</b>
-                        <div className={s.Doctor_avatar_info + " " + s.black}>
-                            <StarComponent func={Star2Action} />
-                        </div>
-                    </li>
-                    <li>
-                        <b className={s.Font_size24}>Отношение к пациенту</b>
-                        <div className={s.Doctor_avatar_info + " " + s.black}>
-                            <StarComponent func={Star3Action} />
-                        </div>
-                    </li>
-                    <li>
-                        <b className={s.Font_size24}>Информирование пациента</b>
-                        <div className={s.Doctor_avatar_info + " " + s.black}>
-                            <StarComponent func={Star4Action} />
-                        </div>
-                    </li>
-                    <li>
-                        <b className={s.Font_size24}>Посоветуете ли Вы врача?</b>
-                        <div className={s.Doctor_avatar_info + " " + s.black}>
-                            <StarComponent func={Star5Action} />
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div className={s.Rewiew_add}>
-                <form onSubmit={(e) => sendForm(e)}>
-                    <div className={s.select_consultation}>
-                        <p className={s.Font_size24}>Выберите консультацию</p>
-                        <SelectReview array={review.consultations} />
-                    </div>
-                        <p className={s.Font_size24}>Ваша история</p>
-                        <textarea name="comment" placeholder="Расскажите, как обратились к врачу, как прошла консультация, помогло ли лечение" />
-                    <div className={s.select_consultation + " " + s.select_margin}>
-                        <p className={s.Font_size24}>Понравилось</p>
-                        <textarea name="like" placeholder="Здесь можно указать главные плюсы" />
-                    </div>
-                    <div className={s.select_consultation + " " + s.select_margin}>
-                        <p className={s.Font_size24}>Не понравилось</p>
-                        <textarea name="not_like" placeholder="Какие недостатки вы отметили?" />
-                    </div>
-                    <div className={s.otziv}>
-                        <button type={'submit'} className={s.Font_size14}>Оставить отзыв</button>
-                    </div>
-                </form>
-            </div>
+  let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(axiosReviews(4));
+  }, []);
+  const review = useSelector((state) => state.review);
+  const sendForm = async (e) => {
+    e.preventDefault();
+    const data = await new FormData(e.target),
+      obj = {};
+    [...data].forEach((e) => {
+      obj[e[0]] = e[1];
+    });
+    console.log({ ...obj });
+  };
+  return (
+    <div className={s.Review_full}>
+      <div className={s.Review_title}>
+        <h1 className={s.Font_size24}>Оставить отзыв</h1>
+      </div>
+      <div className={s.Doctor_infos}>
+        <div className={s.Doctor_avatar}>
+          <div className={s.Doctor_avatar_img}>
+            <img src={review.photo} alt="" />
+          </div>
         </div>
-    )
-}
+        <div className={s.Doctor_info + " " + s.black}>
+          <p className={s.gray + " " + s.Font_size14}>
+            {review.specialization.join(" • ")}
+          </p>
+          <h2 className={s.Font_size24}>
+            {review.firstname + " " + review.lastname + " " + review.secondname}
+          </h2>
+          <p className={s.Staj + " " + s.Font_size14}>
+            {review.regalia.join(" • ")}
+          </p>
+        </div>
+      </div>
+      <div className={s.Grade}>
+        <ul>
+          <li>
+            <b className={s.Font_size24}>Тщательное обследование</b>
+            <div className={s.Doctor_avatar_info + " " + s.black}>
+              <StarComponent func={Star1Action} />
+            </div>
+          </li>
+          <li>
+            <b className={s.Font_size24}>Эффективность лечения</b>
+            <div className={s.Doctor_avatar_info + " " + s.black}>
+              <StarComponent func={Star2Action} />
+            </div>
+          </li>
+          <li>
+            <b className={s.Font_size24}>Отношение к пациенту</b>
+            <div className={s.Doctor_avatar_info + " " + s.black}>
+              <StarComponent func={Star3Action} />
+            </div>
+          </li>
+          <li>
+            <b className={s.Font_size24}>Информирование пациента</b>
+            <div className={s.Doctor_avatar_info + " " + s.black}>
+              <StarComponent func={Star4Action} />
+            </div>
+          </li>
+          <li>
+            <b className={s.Font_size24}>Посоветуете ли Вы врача?</b>
+            <div className={s.Doctor_avatar_info + " " + s.black}>
+              <StarComponent func={Star5Action} />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className={s.Rewiew_add}>
+        <form onSubmit={(e) => sendForm(e)}>
+          <div className={s.select_consultation}>
+            <p className={s.Font_size24}>Выберите консультацию</p>
+            <SelectReview array={review.consultations} />
+          </div>
+          <p className={s.Font_size24}>Ваша история</p>
+          <textarea
+            name="comment"
+            placeholder="Расскажите, как обратились к врачу, как прошла консультация, помогло ли лечение"
+          />
+          <div className={s.select_consultation + " " + s.select_margin}>
+            <p className={s.Font_size24}>Понравилось</p>
+            <textarea
+              name="like"
+              placeholder="Здесь можно указать главные плюсы"
+            />
+          </div>
+          <div className={s.select_consultation + " " + s.select_margin}>
+            <p className={s.Font_size24}>Не понравилось</p>
+            <textarea
+              name="not_like"
+              placeholder="Какие недостатки вы отметили?"
+            />
+          </div>
+          <div className={s.otziv}>
+            <button type={"submit"} className={s.Font_size14}>
+              Оставить отзыв
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default PostRewiew;
--- src/base/asyncActions/RegistrPost.js
+++ src/base/asyncActions/RegistrPost.js
@@ -1,18 +1,20 @@
 import axios from "axios";
-import { defaultUrl } from '../configUrl'
+import { defaultUrl } from "../configUrl";
 
 export const axiosRegister = (obj) => {
   return async function () {
     const data = {};
-    [...obj].forEach(e => {data[e[0]] = e[1]})
-    const token = localStorage.getItem('token');
-    if(token){
-      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+    [...obj].forEach((e) => {
+      data[e[0]] = e[1];
+    });
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     }
-    const response = await axios.post(`${defaultUrl}user`, {...data})
+    const response = await axios.post(`${defaultUrl}user`, { ...data });
     if (response.data.status) {
-      localStorage.setItem('isNewUser', false)
-      return response.data.status
+      localStorage.setItem("isNewUser", false);
+      return response.data.status;
     }
-  }
-}
+  };
+};
