c280e845f7ab7eaad452f0e7568d04841e9957b3
AlexandrKruglenko <fredwolk6@gmail.com>
2022-08-13 13:36:21 +0300

    Модалка \ Уветомления

--- src/Pages/Layout/Header/Header.jsx
+++ src/Pages/Layout/Header/Header.jsx
@@ -7,12 +7,12 @@ import { UserMenu } from "./UserMenu/UserMenu";
 import { useSelector } from "react-redux/es/exports";
 import { Navigate } from "react-router";
 const Header = () => {
-  let Text = useSelector(state => state.config.header_text);
+  let Text = useSelector((state) => state.config.header_text);
   const config = useSelector((state) => state.config.config);
-  console.log(config);
-  return (
-    config && !config.token ?
-      <Navigate to="login" /> : <div className={s.Header_container + " Container"}>
+  return config && !config.token ? (
+    <Navigate to="login" />
+  ) : (
+    <div className={s.Header_container + " Container"}>
       <header className={s.Header_full}>
         <div className={s.Header_logo}>
           <Link to={"main"}>
