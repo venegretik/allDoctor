48285b091d492ad73684ed608d9a937234bc03e0
AlexandrKruglenko <fredwolk6@gmail.com>
2022-08-15 14:13:05 +0300

    Модалка записи

--- src/Components/Modal/Cancel_record/Cancel_Record.jsx
+++ src/Components/Modal/Cancel_record/Cancel_Record.jsx
@@ -1,53 +1,80 @@
 import React from "react";
-import s from './Cancel_Record.module.css';
+import s from "./Cancel_Record.module.css";
 import { axiosConsultationDelete } from "../../../base/asyncActions/getConsultation";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import Button from "../../Button/Button";
 const Cancel_Record = (props) => {
-    let dispatch = useDispatch();
-    let [showWindow, setWindow] = useState(false);
-    let DoctorDelete = (consultation_id) => {
-        dispatch(props.func(consultation_id));
-        setWindow(false)
-    }
-    const config = useSelector((state) => state.config.config);
-    return (
-        <div>
-            <div className={s.Cart_close + " " + s.black} onClick={e => setWindow(true)}>
-                +
+  let dispatch = useDispatch();
+  let [showWindow, setWindow] = useState(false);
+  let DoctorDelete = (consultation_id) => {
+    dispatch(props.func(consultation_id));
+    setWindow(false);
+  };
+  const config = useSelector((state) => state.config.config);
+  return (
+    <div>
+      <div
+        className={s.Cart_close + " " + s.black}
+        onClick={(e) => setWindow(true)}
+      >
+        +
+      </div>
+      {showWindow ? (
+        <div className={s.Cancel_Record_full}>
+          <div className={s.Cancel_Record_block}>
+            <div className={s.ImgNone}>
+              <img
+                src="https://api.telemed.dev-h.ru/images/ui/doc3.png"
+                alt=""
+              />
             </div>
-            {showWindow ? <div className={s.Cancel_Record_full}>
-                <div className={s.Cancel_Record_block}>
-                    <div className={s.ImgNone}>
-                        <img src="https://api.telemed.dev-h.ru/images/ui/doc3.png" alt="" />
-                    </div>
-                    <div className={s.Cancel_Record}>
-                        <h1 className={s.Font_size40} style={{ color: config?.config.colors.color2 }}>{props.text}</h1>
-                        {props.typeModal == "record" ? <p className={s.Font_size16} style={{ color: config?.config.colors.color2 }}>После удаления врача он не будет иметь доступ к вашей медицинской карте</p> : ""}
-                        <div className={s.Cancel_Record_button}>
-                            <div onClick={() => DoctorDelete(props.consultation_id)}>
-                                <Button
-                                    className={s.Font_size16}
-                                    type={'submit'}
-                                    class={'btn orange'}
-                                    text={'Да'}
-                                />
-                            </div>
-                            <div onClick={e => setWindow(false)}>
-                                <Button
-                                    className={s.Font_size16}
-                                    type={'submit'}
-                                    class={'btn white'}
-                                    text={'Нет'}
-                                />
-                            </div>
-                        </div>
-                    </div>
+            <div className={s.Cancel_Record}>
+              <div onClick={() => setWindow(false)} className={s.Cancel_close}>
+                &times;
+              </div>
+              <h1
+                className={s.Font_size40}
+                style={{ color: config?.config.colors.color2 }}
+              >
+                {props.text}
+              </h1>
+              {props.typeModal == "record" ? (
+                <p
+                  className={s.Font_size16}
+                  style={{ color: config?.config.colors.color2 }}
+                >
+                  После удаления врача он не будет иметь доступ к вашей
+                  медицинской карте
+                </p>
+              ) : (
+                ""
+              )}
+              <div className={s.Cancel_Record_button}>
+                <div onClick={() => DoctorDelete(props.consultation_id)}>
+                  <Button
+                    className={s.Font_size16}
+                    type={"submit"}
+                    class={"btn orange"}
+                    text={"Да"}
+                  />
                 </div>
-            </div> : ""}
-
+                <div onClick={(e) => setWindow(false)}>
+                  <Button
+                    className={s.Font_size16}
+                    type={"submit"}
+                    class={"btn white"}
+                    text={"Нет"}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 export default Cancel_Record;
