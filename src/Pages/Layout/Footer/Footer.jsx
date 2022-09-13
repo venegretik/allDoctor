0baa121f1a186acf41d9b91c88b49f391a65b9a8
venegretik <don.artur16084353@gmail.com>
2022-09-13 17:16:20 +0300

    footer

--- src/Pages/Layout/Footer/Footer.jsx
+++ src/Pages/Layout/Footer/Footer.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import s from "./Footer.module.css";
-import Doctor from "../../../img/Doctor_icon.png";
-import Note from "../../../img/note_icon.png";
-import Home from "../../../img/Home_icon.png";
-import Prof from "../../../img/Profile_icon.png";
-import DoctorAct from "../../../img/Doctor_active.png";
-import NoteAct from "../../../img/note_active.png";
-import HomeAct from "../../../img/Home_active.png";
-import ProfAct from "../../../img/profile_active.png";
-import {NavLink} from "react-router-dom";
-import { useSelector} from "react-redux";
+import { ReactComponent as Doctor } from "../../../img/Doctor_icon.svg";
+import { ReactComponent as Note } from "../../../img/note_icon.svg";
+import { ReactComponent as Home } from "../../../img/Home_icon.svg";
+import { ReactComponent as Prof } from "../../../img/Profile_icon.svg";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ChatModal from "../../../Components/Modal/Chat_Modal/ChatModal";
 const Footer = () => {
   let config = useSelector(state => state.config.config);
   const info = useSelector((state) => state.main.shortName);
   let Text = useSelector((state) => state.config.header_text);
   const availableScreenWidth = window.screen.availWidth;
-  return availableScreenWidth <= 480 && Text === "Видео" ? ("") : info?.data.short_name  ?(
+  return availableScreenWidth <= 480 && Text === "Видео" ? ("") : info?.data.short_name ? (
     <section className={s.footer_container + " BackgroundBlue"}>
       <footer className={s.footer_full}>
         <div
@@ -30,7 +26,7 @@ const Footer = () => {
           className="Container"
         >
           <div className={s.footer_img}>
-            <img alt="" src={config.config.logo_footer}  />
+            <img alt="" src={config.config.logo_footer} />
           </div>
           <div className={s.footer_text + " white_config"}>
             <p>© ВсеДоктора 2022</p>
@@ -40,38 +36,38 @@ const Footer = () => {
       <div className={s.footer_Mobile}>
         <ul>
           <NavLink to={"main"} className={({ isActive }) =>
-                isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
-              }>
+            isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
+          }>
             <li>
-              {Text === "Главная" ? <img alt="" src={Home}  /> :<img alt="" src={HomeAct}  />}
-              <p>Главная</p>
+              <Home stroke={Text === "Главная" ? config.config.colors.color10 : config.config.colors.color4} />
+              <p className={Text === "Главная" ? "blue_config" : "gray_config"}>Главная</p>
             </li>
           </NavLink>
           <NavLink to={"consultation"} className={({ isActive }) =>
-                isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
-              }>
+            isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
+          }>
             <li>
-            {Text === "Мои записи" ? <img alt="" src={NoteAct}  /> : <img alt="" src={Note}  />}
-              <p>Мои записи</p>
+              <Note fill={Text === "Мои записи" ? config.config.colors.color10 : config.config.colors.color4} />
+              <p className={Text === "Мои записи" ? "blue_config" : "gray_config"}>Мои записи</p>
             </li>
           </NavLink>
           <li className={s.navLink}>
             <ChatModal />
           </li>
           <NavLink to={"my-doctor"} className={({ isActive }) =>
-                isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
-              }>
+            isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
+          }>
             <li>
-            {Text === "Мои врачи" ? <img alt="" src={DoctorAct}  /> : <img alt="" src={Doctor}  />}
-              <p>Мои врачи</p>
+              <Doctor stroke={Text === "Мои врачи" ? config.config.colors.color10 : config.config.colors.color4}/>
+              <p className={Text === "Мои врачи" ? "blue_config" : "gray_config"}>Мои врачи</p>
             </li>
           </NavLink>
           <NavLink to={"profile/local-data"} className={({ isActive }) =>
-                isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
-              }>
+            isActive ? `${s.navLink} ${s.headerActiveLink}` : s.navLink
+          }>
             <li>
-            {Text === "Профиль" ? <img alt="" src={ProfAct}  /> : <img alt="" src={Prof}  />}
-              <p>Мой кабинет</p>
+              <Prof fill={Text === "Профиль" ? config.config.colors.color10 : config.config.colors.color4} />
+              <p className={Text === "Профиль" ? "blue_config" : "gray_config"}>Мой кабинет</p>
             </li>
           </NavLink>
         </ul>
